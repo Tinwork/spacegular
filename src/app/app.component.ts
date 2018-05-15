dad72e71f3260e43dad364c7dd63de32ea72093c
Antoine Renault <antoine.renault.mmi@gmail.com>
2018-05-15 10:58:00 +0200

    Update: following Bader course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SpacexApiService } from './Providers/Backend/spacex-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(
+    private spacexApi: SpacexApiService
+  ) { }
+
+  ngOnInit(): void {
+    this.spacexApi.getCompanyInfo().subscribe(data => {
+      console.log(JSON.stringify(data))
+    })
+  }
 }
